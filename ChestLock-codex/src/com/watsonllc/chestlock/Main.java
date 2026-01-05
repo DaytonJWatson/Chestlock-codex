027aaf1036d9ae0bd9f0d0b0388894d2bbfbcf45
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2026-01-05 14:17:48 -0700

    Remove ConfigMigration class

--- ChestLock-codex/src/com/watsonllc/chestlock/Main.java
+++ ChestLock-codex/src/com/watsonllc/chestlock/Main.java
@@ -6,7 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.watsonllc.chestlock.commands.Commands;
 import com.watsonllc.chestlock.config.Config;
-import com.watsonllc.chestlock.config.ConfigMigration;
 import com.watsonllc.chestlock.config.LocksData;
 import com.watsonllc.chestlock.events.Events;
 import com.watsonllc.chestlock.config.GroupsData;
@@ -27,11 +26,6 @@ public class Main extends JavaPlugin {
 		String currentVersion = this.getDescription().getVersion();
 
 		Config.setup();
-
-                if (!ConfigMigration.ensureCurrentConfig()) {
-                        Events.invalidConfig();
-                        return;
-                }
 
                 LocksData.create();
                 GroupsData.create();
